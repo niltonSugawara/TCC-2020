04e0a2a9d72a5d6f181ddd32a8aaf43b7fcc9533
cordeiro Moura <jose.moura2@fatec.sp.gov.br>
2020-09-26 17:06:57 -0300

    Inicio estrutura

--- src/main/java/br/com/jn/analyze/utils/AnalyzeApplication.java
+++ src/main/java/br/com/jn/analyze/utils/AnalyzeApplication.java
@@ -1,4 +1,4 @@
-package br.com.jn.analyze;
+package br.com.jn.analyze.utils;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
